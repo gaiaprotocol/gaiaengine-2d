fa68b8e4acd34f69ca35d66ca8f981ad8e13651e
yjgaia <yjdev88@gmail.com>
2024-09-14 15:06:51 +0900

    Dev moving

--- lib/dynamic/Movable.d.ts
+++ lib/dynamic/Movable.d.ts
@@ -1,6 +1,9 @@
 import GameObject from "../core/GameObject.js";
 export default class Movable extends GameObject {
     speedX: number;
+    speedY: number;
+    move(angle: number, speed: number): void;
+    stop(): void;
     protected update(deltaTime: number): void;
 }
 //# sourceMappingURL=Movable.d.ts.map
--- src/dynamic/Movable.ts
+++ src/dynamic/Movable.ts
@@ -2,9 +2,21 @@ import GameObject from "../core/GameObject.js";
 
 export default class Movable extends GameObject {
   public speedX: number = 0;
+  public speedY: number = 0;
+
+  public move(angle: number, speed: number): void {
+    this.speedX = Math.cos(angle) * speed;
+    this.speedY = Math.sin(angle) * speed;
+  }
+
+  public stop(): void {
+    this.speedX = 0;
+    this.speedY = 0;
+  }
 
   protected update(deltaTime: number): void {
     this.x += this.speedX * deltaTime;
+    this.y += this.speedY * deltaTime;
     super.update(deltaTime);
   }
 }
