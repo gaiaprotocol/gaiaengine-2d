3d659068c162372765d625a2e64fcc2239f629dc
yjgaia <yjdev88@gmail.com>
2025-01-08 22:33:17 +0900

    Refactor TerrainDirection enum to reorder direction values for improved clarity and consistency

--- src/map/TerrainDirection.ts
+++ src/map/TerrainDirection.ts
@@ -3,25 +3,25 @@ enum TerrainDirection {
   TopLeft = "top-left",
   Top = "top",
   TopRight = "top-right",
-  Left = "left",
   Right = "right",
-  BottomLeft = "bottom-left",
-  Bottom = "bottom",
   BottomRight = "bottom-right",
+  Bottom = "bottom",
+  BottomLeft = "bottom-left",
+  Left = "left",
 
   // Two-direction fills
   FillTopBottom = "fill-top-bottom",
   FillLeftRight = "fill-left-right",
   FillTopLeft = "fill-top-left",
   FillTopRight = "fill-top-right",
-  FillBottomLeft = "fill-bottom-left",
   FillBottomRight = "fill-bottom-right",
+  FillBottomLeft = "fill-bottom-left",
 
   // Three-direction fills
   FillTopLeftRight = "fill-top-left-right",
-  FillTopLeftBottom = "fill-top-left-bottom",
   FillTopRightBottom = "fill-top-right-bottom",
   FillBottomLeftRight = "fill-bottom-left-right",
+  FillTopLeftBottom = "fill-top-left-bottom",
 
   // All directions filled
   FillFull = "fill-full",
