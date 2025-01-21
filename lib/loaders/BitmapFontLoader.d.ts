d1694207ff27bfd769ebb17e31740debd4be5f62
yjgaia <yjdev88@gmail.com>
2025-01-21 20:31:52 +0900

    Implement BitmapFont and BitmapFontLoader; add BitmapTextNode for text rendering with bitmap fonts

--- lib/loaders/BitmapFontLoader.d.ts
+++ lib/loaders/BitmapFontLoader.d.ts
@@ -1,1 +1,10 @@
+import { ResourceLoader } from "@common-module/ts";
+import BitmapFont from "../text/BitmapFont.js";
+declare class BitmapFontLoader extends ResourceLoader<BitmapFont> {
+    private fntToSrc;
+    protected loadResource(fnt: string, src: string): Promise<BitmapFont | undefined>;
+    protected cleanup(_: BitmapFont, fnt: string): void;
+}
+declare const _default: BitmapFontLoader;
+export default _default;
 //# sourceMappingURL=BitmapFontLoader.d.ts.map
--- lib/text/BitmapTextNode.d.ts
+++ lib/text/BitmapTextNode.d.ts
@@ -1,6 +1,13 @@
-import { BitmapText } from "pixi.js";
-import DisplayNode from "../core/DisplayNode.js";
-export default class BitmapTextNode extends DisplayNode<BitmapText> {
-    constructor(x: number, y: number, text: string);
+import GameObject from "../core/GameObject.js";
+interface BitmapTextNodeOptions {
+    fnt: string;
+    src: string;
 }
+export default class BitmapTextNode extends GameObject {
+    private text;
+    private options;
+    constructor(x: number, y: number, text: string, options: BitmapTextNodeOptions);
+    private loadFont;
+}
+export {};
 //# sourceMappingURL=BitmapTextNode.d.ts.map
