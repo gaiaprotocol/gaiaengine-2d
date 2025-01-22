419d7ca7aa7b967b9d23072e976277236482d813
yjgaia <yjdev88@gmail.com>
2025-01-22 14:23:03 +0900

    Update DomTextNode to provide a default type for style parameter

--- lib/text/DomTextNode.d.ts
+++ lib/text/DomTextNode.d.ts
@@ -1,5 +1,5 @@
 import DomWrapperNode from "../dom/DomWrapperNode.js";
-export default class DomTextNode<ST extends Partial<CSSStyleDeclaration>> extends DomWrapperNode {
+export default class DomTextNode<ST extends Partial<CSSStyleDeclaration> = {}> extends DomWrapperNode {
     constructor(x: number, y: number, text: string, style: ST);
     set text(text: string);
     get text(): string;
